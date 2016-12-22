52e34ba35542c35c28992332564e1490f5ed6092
Ruowen Wang <wangruowen@gmail.com>
2016-12-22 15:04:22 -0800

    work on 4 median of two arrays.

--- LeetCode/4_Median_of_Two_Sorted_Arrays.py
+++ LeetCode/4_Median_of_Two_Sorted_Arrays.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/median-of-two-sorted-arrays/
 
+import math
+
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -11,26 +13,32 @@ class Solution(object):
             self.short_one, self.long_one = nums1, nums2
         else:
             self.short_one, self.long_one = nums2, nums1
-        total_nums = len(self.short_one) + len(self.long_one)
-        is_odd = True if total_nums % 2 == 1 else False
+        self.is_odd = True if (len(self.short_one) + len(self.long_one)) % 2 == 1 else False
 
         return self.findMedianHelper(0, len(self.short_one) - 1, 0, len(self.long_one) - 1)
 
 
     def findMedianHelper(self, start1, end1, start2, end2):
-        # First handle stop case
         mid1 = self.getMedian(self.short_one, start1, end1)
         mid2 = self.getMedian(self.long_one, start2, end2)
 
-        if mid1 < mid2:
-            if start1 + 1 == end1:
-
+        if start1 == end1 and start2 == end2:
+            # We get to the stop point
+            if self.is_odd:
+                return mid1 if mid1 > mid2 else mid2
             else:
-                self.findMedianHelper((start1 + end1) / 2, end1,
-                                      start2, (start2 + end2) / 2)
+                return (mid1 + mid2) / 2.0
+
+        if mid1 < mid2:
+            new_start1 = int(math.ceil((start1 + end1) / 2.0))
+            new_end2 = int(math.floor((start2 + end2) / 2.0))
+            self.findMedianHelper(new_start1, end1,
+                                      start2, new_end2)
         elif mid1 > mid2:
-            self.findMedianHelper(start1, (start1 + end1) / 2,
-                                  (start2 + end2) / 2, end2)
+            new_end1 = int(math.floor((start1 + end1) / 2.0))
+            new_start2 = int(math.ceil((start2 + end2) / 2.0))
+            self.findMedianHelper(start1, new_end1,
+                                  new_start2, end2)
         else:
             return mid1
 
@@ -42,6 +50,6 @@ class Solution(object):
         is_odd = True if (end_index - start_index) % 2 == 0 else False
         mid = (start_index + end_index) / 2
         if is_odd:
-            return array[mid]
+            return float(array[mid])
         else:
-            return (array[mid] + array[mid + 1]) / 2
+            return (array[mid] + array[mid + 1]) / 2.0
