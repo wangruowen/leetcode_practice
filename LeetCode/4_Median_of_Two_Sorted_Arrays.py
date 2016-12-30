14cbdf3a8d383120b7c85f87da593104ed4f9bdd
Ruowen Wang <wangruowen@gmail.com>
2016-12-29 16:55:20 -0800

    work on 4 median of two arrays.

--- LeetCode/4_Median_of_Two_Sorted_Arrays.py
+++ LeetCode/4_Median_of_Two_Sorted_Arrays.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/median-of-two-sorted-arrays/
-
-import math
+# For this question, we actually cannot separately deal with two arrays, because we may get lost in even/odd numbers.
+# The following solution is kinda wrong.
 
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
@@ -15,41 +15,45 @@ class Solution(object):
             self.short_one, self.long_one = nums2, nums1
         self.is_odd = True if (len(self.short_one) + len(self.long_one)) % 2 == 1 else False
 
-        return self.findMedianHelper(0, len(self.short_one) - 1, 0, len(self.long_one) - 1)
+        return self.findMedianHelper(0, len(self.short_one), 0, len(self.long_one))
 
 
     def findMedianHelper(self, start1, end1, start2, end2):
-        mid1 = self.getMedian(self.short_one, start1, end1)
-        mid2 = self.getMedian(self.long_one, start2, end2)
+        if end1 - start1 <= 3 and end2 - start2 <= 3:
+            return self.solveSimpleCase(start1, end1, start2, end2)
+        else:
+            mid1_index, mid2_index = (start1 + end1) / 2, (start2 + end2) / 2
+            mid1, mid2 = self.short_one[mid1_index], self.long_one[mid2_index]
 
-        if start1 == end1 and start2 == end2:
-            # We get to the stop point
-            if self.is_odd:
-                return mid1 if mid1 > mid2 else mid2
+            # If start + end is even,
+            # return the mid as the first item in the second half of the array;
+            # If start + end is odd,
+            # return the exact mid item of the array
+            if mid1 < mid2:
+                return self.findMedianHelper(mid1_index, end1, start2, mid2_index)
+            elif mid1 > mid2:
+                return self.findMedianHelper(start1, mid1_index, mid2_index, end2)
             else:
-                return (mid1 + mid2) / 2.0
+                return mid1
 
-        if mid1 < mid2:
-            new_start1 = int(math.ceil((start1 + end1) / 2.0))
-            new_end2 = int(math.floor((start2 + end2) / 2.0))
-            self.findMedianHelper(new_start1, end1,
-                                      start2, new_end2)
-        elif mid1 > mid2:
-            new_end1 = int(math.floor((start1 + end1) / 2.0))
-            new_start2 = int(math.ceil((start2 + end2) / 2.0))
-            self.findMedianHelper(start1, new_end1,
-                                  new_start2, end2)
+
+    def solveSimpleCase(self, start1, end1, start2, end2):
+        if end1 - start1 == 0:
+            tmp1 = self.short_one[start1:(start1 + 1)]
         else:
-            return mid1
-
-
-    def getMedian(self, array, start_index, end_index):
-        if start_index == end_index:
-            return array[start_index]
-
-        is_odd = True if (end_index - start_index) % 2 == 0 else False
-        mid = (start_index + end_index) / 2
-        if is_odd:
-            return float(array[mid])
+            tmp1 = self.short_one[start1:end1]
+        if end2 - start2 == 0:
+            tmp2 = self.long_one[start2:(start2 + 1)]
         else:
-            return (array[mid] + array[mid + 1]) / 2.0
+            tmp2 = self.long_one[start2:end2]
+        tmp = sorted(tmp1 + tmp2)
+        mid = tmp[len(tmp) / 2] if len(tmp) % 2 == 1 else (tmp[len(tmp) / 2 - 1] + tmp[len(tmp) / 2]) / 2.0
+
+        return float(mid)
+
+
+a = [3,4,5,6,7,8]
+b = [0, 1, 2]
+
+s = Solution()
+print(s.findMedianSortedArrays(a, b))
