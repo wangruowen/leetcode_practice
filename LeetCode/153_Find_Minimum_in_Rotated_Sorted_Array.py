a6b0e86998b6953bc1b5fe5075186dd841b7ac53
wangruowen <wangruowen@gmail.com>
2017-08-26 14:18:08 -0700

    Add binary search for 153

--- LeetCode/153_Find_Minimum_in_Rotated_Sorted_Array.py
+++ LeetCode/153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -16,5 +16,27 @@ class Solution(object):
 
         return nums[0]
 
+    def findMin_v2(self, nums):
+        """
+        Use binary search
+        :param nums:
+        :return:
+        """
+        start, end = 0, len(nums) - 1
+        # If end is within the array index, we usually use "start <= end" in while condition,
+        # instead of "start < end"
+        while start <= end:
+            mid = (start + end) / 2
+            if mid > 0 and nums[mid - 1] > nums[mid]:
+                return nums[mid]
+
+            if nums[mid] > nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+        return nums[0]
+
+
 s = Solution()
-print(s.findMin([4, 5, 6, 7, 0, 1, 2]))
+print(s.findMin_v2([4, 5, 6, 7, 0, 1, 2]))
