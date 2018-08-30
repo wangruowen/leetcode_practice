a7ded11b2f9c5a060163808c058c39d3e237fe7e
Ruowen Wang <wangruowen@gmail.com>
2018-08-29 23:03:39 -0700

    Daily commit

--- LeetCode/684_Redundant_Connection.py
+++ LeetCode/684_Redundant_Connection.py
@@ -7,30 +7,42 @@ class Solution:
         :type edges: List[List[int]]
         :rtype: List[int]
         """
-        # Find the cycle
+        # Find the cycle or we can use Union-Find to solve
         adjacency_matrix = defaultdict(list)
         for u, v in edges:
             adjacency_matrix[u].append(v)
+            adjacency_matrix[v].append(u)
 
-        def DFS(i, matrix, visited_edges, dup):
-            if i not in matrix:
-                return
+        def DFS(i, visited_nodes, visited_edges=set()):
+            if i not in adjacency_matrix:
+                return None
 
-            for j in matrix[i]:
-                if j in result:
-                    dup.append(j)
-                    return
-                    visited_edges.append(j)
-                DFS(j, matrix, visited_edges, dup)
+            for j in adjacency_matrix[i]:
+                cur_edge = (i, j) if i < j else (j, i)
+                if cur_edge in visited_edges:
+                    # Already visited this edge, skip
+                    continue
+                visited_edges.add(cur_edge)
+                if j in visited_nodes:
+                    # Reach already-visited node j from another edge
+                    return visited_nodes[visited_nodes.index(j):]
+                visited_nodes.append(j)
+                found = DFS(j, visited_nodes, visited_edges)
+                if found:
+                    return found
+
+                # Backtrack
+                visited_edges.remove(cur_edge)
+                visited_nodes.pop()
+            return None
 
         for i in adjacency_matrix:
-            result, dup = [], []
-            DFS(i, adjacency_matrix, result, dup)
-            if len(dup) > 0:
+            found = DFS(i, [i])
+            if found:
                 # Found cycle
                 # print(result)
                 for u, v in edges[::-1]:
-                    if u in result and v in result:
+                    if u in found and v in found:
                         return [u, v]
 
         return None
